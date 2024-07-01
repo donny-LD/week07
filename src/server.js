667288f98ffc95d534fc5081ea743f18a2f5c929
mukhtar mohamed <170721974+donny-LD@users.noreply.github.com>
2024-07-01 11:36:17 +0100

    added extra html page

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require ("express");
 
 const app = express ();
 
+app.use("/example", express.static("example"));
+
 app.listen(5001,()=>{
     console.log(`Server listening of port 5001`);
 });
